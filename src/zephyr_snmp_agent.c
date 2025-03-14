0f9ff3d0c810660026e6221e5b5db3fd422704d9
Lukas Greiner <lukas.greiner@codewrights.de>
2025-03-14 10:12:30 +0100

    added system description initialization to snmp agent

--- src/zephyr_snmp_agent.c
+++ src/zephyr_snmp_agent.c
@@ -5,8 +5,13 @@
 
 #include <zephyr/kernel.h>
 
+#include <lwip/opt.h>
+
 void snmp_init(void);
 void snmp_loop();
+
+void snmp_mib2_set_syscontact(u8_t *ocstr, u16_t *ocstrlen, u16_t bufsize);
+void snmp_mib2_set_syslocation(u8_t *ocstr, u16_t *ocstrlen, u16_t bufsize);
 
 static void zephyr_snmp_agent(void *data0, void *data1, void *data2);
 
@@ -19,6 +24,16 @@ static void zephyr_snmp_agent(void *data0, void *data1, void *data2)
     ARG_UNUSED(data0);
     ARG_UNUSED(data1);
     ARG_UNUSED(data2);
+
+    u8_t syscontact[] = "admin@example.com";
+    u16_t syscontact_len = sizeof(syscontact) - 1;
+    u16_t syscontact_bufsize = sizeof(syscontact);
+    snmp_mib2_set_syscontact(syscontact, &syscontact_len, syscontact_bufsize);
+
+    u8_t syslocation[] = "Example Room";
+    u16_t syslocation_len = sizeof(syslocation) - 1;
+    u16_t syslocation_bufsize = sizeof(syslocation);
+    snmp_mib2_set_syslocation(syslocation, &syslocation_len, syslocation_bufsize);
 
     snmp_init();
 
