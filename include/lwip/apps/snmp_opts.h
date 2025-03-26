140b3775cba8401a7323f6acf8d2b09c7408ad4b
Lukas Greiner <lukas.greiner@codewrights.de>
2025-03-26 17:29:40 +0100

    moved the maximum paket size from the macro to the max octed string

--- include/lwip/apps/snmp_opts.h
+++ include/lwip/apps/snmp_opts.h
@@ -141,7 +141,7 @@
 /**
  * The maximum size of a value.
  */
-#define SNMP_MAX_VALUE_SIZE             LWIP_MAX(LWIP_MAX(LWIP_MAX((SNMP_MAX_OCTET_STRING_LEN), sizeof(u32_t)*(SNMP_MAX_OBJ_ID_LEN)), SNMP_MIN_VALUE_SIZE), 256)
+#define SNMP_MAX_VALUE_SIZE             LWIP_MAX(LWIP_MAX((SNMP_MAX_OCTET_STRING_LEN), sizeof(u32_t)*(SNMP_MAX_OBJ_ID_LEN)), SNMP_MIN_VALUE_SIZE)
 #endif
 
 /**
