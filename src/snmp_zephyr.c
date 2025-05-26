2b9f4a5bcd62b14039b69ed2c4c105993ea7fdcb
Lukas Greiner <lukas.greiner@codewrights.de>
2025-05-26 15:54:30 +0200

    Removed example settings

--- src/snmp_zephyr.c
+++ src/snmp_zephyr.c
@@ -402,21 +402,6 @@ static void zephyr_snmp_agent(void *data0, void *data1, void *data2)
     u16_t sysdescr_len = strlen(sysdescr);
     snmp_mib2_set_sysdescr(sysdescr, &sysdescr_len);
 
-    u8_t syscontact[256] = "admin@example.com";
-    u16_t syscontact_len = strlen(syscontact);
-    u16_t syscontact_bufsize = sizeof(syscontact);
-    snmp_mib2_set_syscontact(syscontact, &syscontact_len, syscontact_bufsize);
-    
-    u8_t sysname[256] = "Example Name";
-    u16_t sysname_len = strlen(sysname);
-    u16_t sysname_bufsize = sizeof(sysname);
-    snmp_mib2_set_sysname(sysname, &sysname_len, sysname_bufsize);
-
-    u8_t syslocation[256] = "Example Room";
-    u16_t syslocation_len = strlen(syslocation);
-    u16_t syslocation_bufsize = sizeof(syslocation);
-    snmp_mib2_set_syslocation(syslocation, &syslocation_len, syslocation_bufsize);
-
     snmp_init();
 
     while (1)
